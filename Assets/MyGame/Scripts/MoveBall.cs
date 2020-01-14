605f30afb9568d1f1657ef3da12cce8187e6eeef
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2020-01-14 15:09:19 +0100

    Script

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public GameObject ballPosTopLeft;
+    public GameObject ballPosTopRight;
+    public GameObject ballPosBottomLeft;
+    public GameObject ballPosBottomRight;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject ball;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+}
